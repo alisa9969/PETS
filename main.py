9e16e655a68e44bee309a93c75ef01c1ee3326d0
allis <alisa.mustyatsa20@yandex.ru>
2024-03-31 03:19:39 +0500

    профиль и категории

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data import db_session
 from data.users import User
 from forms.user import RegisterForm
 from forms.login import LoginForm
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__, static_folder="static")
 app.config['SECRET_KEY'] = 'pets.website_secret_key'
@@ -22,36 +22,44 @@ def load_user(user_id):
 def index():
     return render_template('index.html')
 
+@app.route('/category')
+def category():
+    return render_template('category.html')
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    if current_user.is_authenticated:
+        return render_template('profile.html')
+    return render_template('autorization.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return render_template('profile.html')
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.phone == form.phone.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/")
+            return redirect("/profile")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/autorization')
-def autoriz():
-    return render_template('autorization.html')
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return render_template('profile.html')
     form = RegisterForm()
     if form.validate_on_submit():
+        if len(form.password.data) <= 6:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Минимальная длина пароля: 6")
         if form.password.data != form.password_again.data:
             return render_template('register.html', title='Регистрация',
                                    form=form,
